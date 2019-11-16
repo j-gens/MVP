f3bc4f17fcde5a18ea35660260c9c1dd7bb936bf
j-gens <jgens2@gmail.com>
2019-11-16 15:07:59 -0800

    Add top navigation bar

---
+++ client/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navigation = (props) => (
+  <div className="header-navigation">
+    <div className="nav-item">
+      Quiz
+    </div>
+    <div className="nav-item">
+      ||
+    </div>
+    <div className="nav-item">
+      Routine
+    </div>
+    <div className="nav-item">
+      ||
+    </div>
+    <div className="nav-item">
+      Ingredients
+    </div>
+  </div>
+)
+
+export default Navigation;
