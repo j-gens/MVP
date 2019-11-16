e7938570029547f945880930586fc29cf18e0002
j-gens <jgens2@gmail.com>
2019-11-16 11:47:12 -0800

    Set up skeleton crud routes

--- server/index.js
+++ server/index.js
@@ -5,5 +5,21 @@ const port = 4000;
 
 app.use(express.static('public'));
 
+app.get('/api/', (req, res) => {
+
+})
+
+app.post('/api/', (req, res) => {
+
+})
+
+app.put('/api/', (req, res) => {
+
+})
+
+app.delete('/api', (req, res) => {
+
+})
+
 app.listen(port, () => console.log('listening...'))
 
