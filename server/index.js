ddd0ad39001c0cf71cbc54c3041c10711da1de02
j-gens <jgens2@gmail.com>
2019-11-16 11:43:00 -0800

    Fix spelling error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = requrie('express');
+const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
-
+app.use(express.static('public'));
 
 app.listen(port, () => console.log('listening...'))
 
