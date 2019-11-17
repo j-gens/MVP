84b756d74e9f23680be598f13f10870590d3b2e0
j-gens <jgens2@gmail.com>
2019-11-16 17:06:52 -0800

    Connect to files made in structure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import Navigation from './Navigation.jsx';
+import Home from './Home.jsx';
+import Quiz from './Quiz/Quiz.jsx';
+import Routine from './Routine/Routine.jsx';
+import Products from './Products/Products.jsx';
+import Ingredients from './Ingredients/Ingredients.jsx';
 
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       navigationBar: ['Home', 'Quiz', 'Routine', 'Products', 'Ingredients'],
       currentDisplay: 'Home'
     }
 
     this.fetch = this.fetch.bind(this);
+    this.navigate = this.navigate.bind(this);
   }
 
   componentDidMount() {
@@ -22,16 +28,38 @@ class App extends React.Component {
     //axios.get()
   }
 
+  navigate(value) {
+    this.setState({
+      currentDisplay: value
+    });
+  }
+
   render() {
+    let display;
+
+    if (this.state.currentDisplay === 'Home') {
+      display = <Home />
+    } else if (this.state.currentDisplay === 'Quiz') {
+      display = <Quiz />
+    }else if (this.state.currentDisplay === 'Routine') {
+      display = <Routine />
+    } else if (this.state.currentDisplay === 'Products') {
+      display = <Products />
+    } else if (this.state.currentDisplay === 'Ingredients') {
+      display = <Ingredients />
+    }
+
     return (
       <div>
         <div className="header-bin">
           <div className="header-title">
-            Skintopeia
+            The Page Header
           </div>
-          <Navigation bar={this.state.navigationBar} length={this.state.navigationBar.length} />
+          <Navigation bar={this.state.navigationBar} length={this.state.navigationBar.length} navigate={this.navigate} />
         </div>
-        <div className="content-bin">Content</div>
+        <div className="content-bin">
+          {display}
+        </div>
       </div>
     );
   }
