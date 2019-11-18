3ced4821dbbb01581af72a6660f35289f840d273
j-gens <jgens2@gmail.com>
2019-11-18 07:03:51 -0800

    Create general schema for usage

---
+++ server/database/schemas.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const arenaSchema = new Schema({
+  _id: Number,
+  arenaName: String,
+  city: String,
+  stateProvince: String,
+  team: String
+})
+
+const userListSchema = new Schema({
+  _id: Number,
+  _userName: String,
+  list: [{
+    arenaName: String,
+    dateDay: Number,
+    dateMo: Number,
+    dateYr: Number,
+    homeTeam: String,
+    awayTeam: String
+  }]
+})
+
+
+const Arena = mongoose.model('Arena', arenaSchema);
+const UserList = mongoose.model('UserList', userListSchema);
+
+module.exports.Arena = Arena;
+module.exports.UserList = UserList;
