a6c761b77aa14559ec62af7bd9c98160d3d10c57
j-gens <jgens2@gmail.com>
2019-11-18 07:04:14 -0800

    Delete the skeleton file system (redesigned front end)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,67 +1,23 @@
 import React from 'react';
 import axios from 'axios';
-import Navigation from './Navigation.jsx';
-import Home from './Home.jsx';
-import Quiz from './Quiz/Quiz.jsx';
-import Routine from './Routine/Routine.jsx';
-import Products from './Products/Products.jsx';
-import Ingredients from './Ingredients/Ingredients.jsx';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      navigationBar: ['Home', 'Quiz', 'Routine', 'Products', 'Ingredients'],
-      currentDisplay: 'Home'
+
     }
-
-    this.fetch = this.fetch.bind(this);
-    this.navigate = this.navigate.bind(this);
   }
 
-  componentDidMount() {
-    //this.fetch();
-  }
-
-  fetch() {
-    //axios.get()
-  }
-
-  navigate(value) {
-    this.setState({
-      currentDisplay: value
-    });
-  }
+  fetch()
 
   render() {
-    let display;
-
-    if (this.state.currentDisplay === 'Home') {
-      display = <Home />
-    } else if (this.state.currentDisplay === 'Quiz') {
-      display = <Quiz />
-    }else if (this.state.currentDisplay === 'Routine') {
-      display = <Routine />
-    } else if (this.state.currentDisplay === 'Products') {
-      display = <Products />
-    } else if (this.state.currentDisplay === 'Ingredients') {
-      display = <Ingredients />
-    }
-
     return (
       <div>
-        <div className="header-bin">
-          <div className="header-title">
-            The Page Header
-          </div>
-          <Navigation bar={this.state.navigationBar} length={this.state.navigationBar.length} navigate={this.navigate} />
-        </div>
-        <div className="content-bin">
-          {display}
-        </div>
+        hello again
       </div>
-    );
+    )
   }
 }
 
--- client/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-  <div>
-    This is the home page
-  </div>
-)
-
-export default Home;
