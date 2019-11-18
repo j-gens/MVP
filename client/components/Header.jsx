22fefb02e728c485f148d9c02fb7ef1ad6925f4a
j-gens <jgens2@gmail.com>
2019-11-18 08:38:54 -0800

    Add header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="header-bin">
+        <div className="header-title">
+          Fear the Fin
+        </div>
+        <div className="header-nav-bin">
+          testing
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
