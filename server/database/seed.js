0f15649bce8c031ba6832d9829e160e7d492a3ec
j-gens <jgens2@gmail.com>
2019-11-18 11:52:28 -0800

    Create seed script for database

---
+++ server/database/seed.js
@@ -0,0 +1,42 @@
+const db = require('./index.js');
+const schema = require('./schemas.js');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+/*
+const arenaSchema = new Schema({
+  _id: Number,
+  arenaName: String,
+  city: String,
+  stateProvince: String,
+  team: String
+})
+*/
+
+let i = 0;
+
+fs.createReadStream('nhl_arenas.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    let arena = new schema.Arena({
+      '_id': i,
+      'arenaName': row.Arena,
+      'city': row.City,
+      'stateProvince': row.State,
+      'team': row.Team
+    });
+    arena.save((err, success) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(success)
+      }
+    });
+    i++;
+  })
+  .on('end', () => {
+    console.log('complete!')
+  })
+
+
+
