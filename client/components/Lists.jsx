18acd4816e69a3ab09d0bedcb976ad2e84596b0a
j-gens <jgens2@gmail.com>
2019-11-18 16:19:17 -0800

    Rough server call

--- client/components/Lists.jsx
+++ client/components/Lists.jsx
@@ -1,29 +1,87 @@
 import React from 'react';
 import axios from 'axios';
-
+import Users from './Lists/Users.jsx';
+import ListContent from './Lists/Content.jsx';
 
 class Lists extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      arenas: [],
+      users: ['jules', 'user1', 'hello'],
+      usersData: [],
+      currentUser: 'createNew'
     }
 
-    this.fetch = this.fetch.bind(this);
+    this.fetchArenas = this.fetchArenas.bind(this);
+    this.fetchUsers = this.fetchUsers.bind(this);
+    this.changeView = this.changeView.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  fetch() {
-    axios.get('/api/lists/')
+  fetchArenas() {
+    axios.get('/api/arenas/')
+      .then((response) => {
+        this.setState({
+          arenas: response.data
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log('success', this.state.arenas);
+        this.fetchUsers();
+      })
+  }
+
+  fetchUsers() {
+    axios.get('api/users')
+      .then((response) => {
+        this.setState({
+          users: response.data
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log('success'. this.state.users);
+      })
+  }
+
+  componentDidMount() {
+    this.fetchArenas();
+  }
+
+  changeView(name) {
+    this.setState({
+      currentUser: name
+    })
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentUser: event.target.value
+    })
   }
 
   render() {
     return (
       <div className="list-bin">
         <div className="list-user-bin">
-          here are the users
+          <div className="list-user-title">
+            Current Users:
+          </div>
+          <div>
+            <div className="list-user-entry" value="createNew" onClick={this.handleClick}>
+              Create a New User
+            </div>
+            <Users users={this.state.users} changeView={this.changeView} />
+          </div>
         </div>
         <div className="list-content-bin">
-          here are the lists
+          <ListContent currentUser={this.state.currentUser} arenas={this.state.arenas} />
         </div>
       </div>
     )
