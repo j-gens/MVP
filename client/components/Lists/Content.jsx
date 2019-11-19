47be90d195f52ef3b254d091bde43db57b9a5c45
j-gens <jgens2@gmail.com>
2019-11-18 18:47:33 -0800

    Update list content component to dynamically render a user's list

--- client/components/Lists/Content.jsx
+++ client/components/Lists/Content.jsx
@@ -3,40 +3,29 @@ import axios from 'axios';
 import ListContentEntry from './ContentEntry.jsx';
 import ContentForm from './ContentForm.jsx';
 
-class ListContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-      <div className="content-list">
-          <div className="arena-form-title">
-            {this.props.currentUser} 's List:
-          </div>
-          <div>
-          <table>
-            <thead className="content-list-table-header">
-              <td>Arena Name</td>
-              <td>Home Team</td>
-              <td>Visiting Team</td>
-              <td>Date of Visit</td>
-            </thead>
-            <tbody>
-              {this.props.arenas.map(arena =>
-                <ListContentEntry arena={arena} />
-              )}
-            </tbody>
-          </table>
-        </div>
-        <ContentForm user={this.props.currentUser} arenas={this.props.arenas} />
+const ListContent = (props) => (
+  <div className="content-list">
+      <div className="arena-form-title">
+        {this.props.currentUser}'s List ({this.props.currentList.length} out of 31):
       </div>
-    );
+      <div>
+      <table>
+        <thead className="content-list-table-header">
+          <td>Arena Name</td>
+          <td>Home Team</td>
+          <td>Visiting Team</td>
+          <td>Date of Visit</td>
+        </thead>
+        <tbody>
+          {this.props.currentList.map(arena =>
+            <ListContentEntry arena={arena} />
+          )}
+        </tbody>
+      </table>
+    </div>
+    <ContentForm getUser={this.props.getUser} user={this.props.currentUser} arenas={this.props.arenas} />
+  </div>
+)
 
-  }
-}
 
 export default ListContent;
