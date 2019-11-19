69f759715056478da5bcfebefa2a1246583fbd85
j-gens <jgens2@gmail.com>
2019-11-18 16:18:46 -0800

    Add additional routes

--- server/index.js
+++ server/index.js
@@ -1,23 +1,60 @@
 const express = require('express');
+const db = require('./database/index.js');
+const models = require('./database/models.js');
 
 const app = express();
 const port = 4000;
 
 app.use(express.static('public'));
 
-app.get('/api/', (req, res) => {
-
+//get a list of all arenas
+app.get('/api/arenas/', (req, res) => {
+  models.getAllArenas((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(data);
+  })
 })
 
-app.post('/api/', (req, res) => {
-
+//get details for an individual arena
+app.get('/api/arena/:arenaId', (req, res) => {
+  console.log('single arena id => ', req.params.arenaId);
+  models.getOneArena(req.params.arenaId, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(data);
+  })
 })
 
+//get list of users
+app.get('api/users', (req, res) => {
+  models.getAllUsers((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(data);
+  })
+})
+
+//save a new user
+app.post('/api/users', (req, res) => {
+  console.log('body ', req.body)
+  models.saveUser(req.body.data, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(data);
+  })
+})
+
+//update a current user list
 app.put('/api/', (req, res) => {
 
 })
 
-app.delete('/api', (req, res) => {
+app.delete('/api/', (req, res) => {
 
 })
 
