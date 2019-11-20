a67a23756dc76a8566422ea2982557869c7ed6ed
j-gens <jgens2@gmail.com>
2019-11-19 17:17:40 -0800

    Add dynamic links to hockey team table

--- client/components/App/ArenaListEntry.jsx
+++ client/components/App/ArenaListEntry.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const ArenaListEntry = (props) => (
-  <tr className="content-list-table-row-top">
-    <td>{props.arena.arenaName}</td>
-    <td>{props.arena.team}</td>
-    <td>{props.arena.city}</td>
-    <td>{props.arena.stateProvince}</td>
-  </tr>
-)
+const ArenaListEntry = (props) => {
+  let endpoint = props.arena.team.split(' ');
+  let hockeyurl = "http://www.nhl.com/" + endpoint[endpoint.length-1].toLowerCase();
+
+  return (
+    <tr className="content-list-table-row-top">
+      <td>{props.arena.arenaName}</td>
+      <td><a href={hockeyurl} target="_blank">{props.arena.team}</a></td>
+      <td>{props.arena.city}</td>
+      <td>{props.arena.stateProvince}</td>
+    </tr>
+  )
+}
+
 
 
 export default ArenaListEntry;
