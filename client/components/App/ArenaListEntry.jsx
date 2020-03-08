f6b1aaa17084b5f2a4786406256b4d125772514c
j-gens <jgens2@gmail.com>
2020-03-07 17:45:54 -0800

    Destructure props and update code spacing to be consistent across files

--- client/components/App/ArenaListEntry.jsx
+++ client/components/App/ArenaListEntry.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const ArenaListEntry = (props) => {
-  let endpoint = props.arena.team.split(' ');
-  let hockeyurl = "http://www.nhl.com/" + endpoint[endpoint.length-1].toLowerCase();
+
+const ArenaListEntry = ({ arena: { team, arenaName, city, stateProvince } }) => {
+  const endpoint = team.split(' ');
+  const hockeyurl = "http://www.nhl.com/" + endpoint[endpoint.length-1].toLowerCase();
 
   return (
     <tr className="content-list-table-row-top">
-      <td>{props.arena.arenaName}</td>
-      <td><a href={hockeyurl} target="_blank">{props.arena.team}</a></td>
-      <td>{props.arena.city}</td>
-      <td>{props.arena.stateProvince}</td>
+      <td>{arenaName}</td>
+      <td><a href={hockeyurl} target="_blank">{team}</a></td>
+      <td>{city}</td>
+      <td>{stateProvince}</td>
     </tr>
-  )
+  );
 }
-
 
 
 export default ArenaListEntry;
