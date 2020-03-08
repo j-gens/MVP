c066ccbba15aaa73870bd1d7d954fd3c379e3ae2
j-gens <jgens2@gmail.com>
2020-03-07 16:26:38 -0800

    Update code spacing for consistency across files

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    }
-  }
+const Header = () => (
+  <div className="header-bin">
+    <div className="header-title">
+      Ice Bucket List
+    </div>
+    <div className="header-sub-title">
+      KEEP TRACK OF ALL THE NHL ARENAS YOU'VE VISITED
+    </div>
+  </div>
+);
 
-  render() {
-    return (
-      <div className="header-bin">
-        <div className="header-title">
-          Ice Bucket List
-        </div>
-        <div className="header-nav-bin">
-          KEEP TRACK OF ALL THE NHL ARENAS YOU'VE VISITED
-        </div>
-      </div>
-    )
-  }
-}
 
 export default Header;
