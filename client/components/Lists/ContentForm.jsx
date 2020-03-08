206925a9656957de828b54c729ae6c89cb8dad64
j-gens <jgens2@gmail.com>
2020-03-07 17:36:25 -0800

    Add destructure and class method syntax

--- client/components/Lists/ContentForm.jsx
+++ client/components/Lists/ContentForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+
 class ContentForm extends React.Component {
   constructor(props) {
     super(props);
@@ -10,40 +11,29 @@ class ContentForm extends React.Component {
       visitors: '',
       date: '  /  /  '
     }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.findHomeTeam = this.findHomeTeam.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleChange(event) {
-    if (event.target.name === "arenas") {
-      this.setState({
-        arena: event.target.value
-      });
-      this.findHomeTeam(event.target.value);
-    } else if (event.target.name === "date") {
-      this.setState({
-        date: event.target.value
-      });
-    } else if (event.target.name === "visitors") {
-      this.setState({
-        visitors: event.target.value
-      })
-    }
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
 
-  findHomeTeam(arena) {
-    for (let i = 0; i < this.props.arenas.length; i++) {
-      if (arena === this.props.arenas[i].arenaName) {
+  findHomeTeam = (arena) => {
+    const { arenas } = this.props;
+    for (let i = 0; i < arenas.length; i++) {
+      if (arena === arenas[i].arenaName) {
         this.setState({
-          home: this.props.arenas[i].team
+          home: arenas[i].team
         })
       }
     }
   }
 
-  handleFormSubmit(event) {
+  handleFormSubmit = (event) => {
+    const { arena, home, visitors, date } = this.state;
+    const { getListByUser, user } = this.props;
     event.preventDefault();
     axios.post('/api/lists', {
       userName: this.props.user,
@@ -52,28 +42,26 @@ class ContentForm extends React.Component {
       awayTeam: this.state.visitors,
       date: this.state.date
       })
-      .then((response) => {
-        this.props.getListByUser(this.props.user);
+      .then(() => {
+        getListByUser(user);
       })
       .catch((err) => {
         console.log(err);
       })
-      .finally(() => {
-        console.log('posted!')
-      })
   }
 
-  render() {
+  render = () => {
+    const { user, arenas } = this.props;
     return (
       <div className="content-form">
         <div className="list-user-title">
-          Add another arena to {this.props.user}'s list:
+          Add another arena to {user}'s list:
         </div>
         <form className="arena-form">
           <label>
             Arena:
-            <select name="arenas" onChange={this.handleChange}>
-              {this.props.arenas.map(arena =>
+            <select name="arena" onChange={this.handleChange}>
+              {arenas.map(arena =>
                 <option value={arena.arenaName}>{arena.arenaName} -- {arena.team}</option>
               )}
             </select>
@@ -96,5 +84,6 @@ class ContentForm extends React.Component {
     );
   }
 }
+
 
 export default ContentForm;
