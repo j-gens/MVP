f8258d9a674e112c886ac1ff15ff674b48e52520
j-gens <jgens2@gmail.com>
2020-03-09 11:52:57 -0700

    Boilerplate class component and styles sheet

---
+++ client/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './sign-up.styles.css';
+
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className='signup-bin'>
+        sign up here
+      </div>
+    );
+  }
+}
+
+
+export default SignUp;
