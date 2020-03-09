50f513a39a0a6e93a9e4f830810bf4826bee3bc4
j-gens <jgens2@gmail.com>
2020-03-09 10:55:17 -0700

    Boilerplate functional component and styles sheet

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './components/header/header.component.jsx';
+
+import './App.css';
+
+
+const App = () => (
+  <div className='app-bin'>
+    <Header />
+  </div>
+);
+
+
+export default App;
