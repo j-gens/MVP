f45d8a69bfd123c45cef4e9b9540e427687ac91c
j-gens <jgens2@gmail.com>
2020-03-09 11:48:47 -0700

    Boilerplate functional component and styles sheet

---
+++ client/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.css';
+
+
+const SignInSignUp = () => (
+  <div className="signin-signup-bin">
+    sign in and sign up here
+  </div>
+);
+
+
+export default SignInSignUp;
