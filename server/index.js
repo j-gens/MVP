457c1cc4deeea4a19cc386bdcc9609205463f4da
j-gens <jgens2@gmail.com>
2020-03-08 19:17:37 -0700

    Separate server code into two files

--- server/index.js
+++ server/index.js
@@ -1,76 +1,9 @@
-const express = require('express');
-const db = require('./database/index.js');
-const models = require('./database/models.js');
-
-const app = express();
-const port = 4000;
-
-app.use(express.static('public'));
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+const app = require('./app');
 
 
-//get a list of all arenas
-app.get('/api/arenas/', (req, res) => {
-  models.getAllArenas((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
+let port = process.env.PORT;
+if (port == '' || port == undefined) {
+  port = 4000;
+}
 
-//get list of all users in database
-app.get('/api/users', (req, res) => {
-  models.getAllUsers((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
-
-//save a new user to the database
-app.post('/api/users', (req, res) => {
-  console.log('post to users ', req.body.userName)
-  models.saveUser(req.body.userName, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
-
-//get a user's list of arenas visited
-app.get('/api/lists/:username', (req, res) => {
-  models.getListByUser(req.params.username, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
-
-//update a current user's list
-app.post('/api/lists', (req, res) => {
-  console.log('is this the data' , req.body)
-  models.saveArenaToList(req.body, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
-
-app.delete('/api/', (req, res) => {
-  models.removeOneFromList(req.body.arenaName, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).send(data);
-  })
-})
-
-app.listen(port, () => console.log('listening...'))
-
+app.listen(port, () => console.log('listening...'));
