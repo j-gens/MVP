bb3747bd058f5137752a442bac5df1cb2b0ec1ad
j-gens <jgens2@gmail.com>
2020-03-09 10:56:19 -0700

    Boilerplate functional component and styles sheet

---
+++ client/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.styles.css';
+
+
+const Header = () => (
+  <div className="header-bin">
+    ICE BUCKET LIST
+  </div>
+);
+
+
+export default Header;
