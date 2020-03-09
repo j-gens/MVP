73ff202acd6310af41679b499f21717c35858ece
j-gens <jgens2@gmail.com>
2020-03-09 10:59:58 -0700

    Boilerplate functional component and styles sheet

---
+++ client/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.css';
+
+
+const HomePage = () => (
+  <div className='homepage-bin'>
+    this is the homepage - welcome!
+  </div>
+);
+
+
+export default HomePage;
