7e19373bace3790dfc3ae93bfa22e81583df697b
j-gens <jgens2@gmail.com>
2020-03-10 16:13:38 -0700

    Boilerplate functional component and styles sheet

---
+++ client/pages/current-arenas/current-arenas.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './current-arenas.styles.css';
+
+
+const CurrentArenas = () => (
+  <div>
+    current arenas here
+  </div>
+);
+
+
+export default CurrentArenas;
