951f6f1d1d9281a69d4fef7629e9dd620169d339
j-gens <jgens2@gmail.com>
2020-03-10 16:14:40 -0700

    Add routing for current arenas list and map component

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header.component.jsx';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx';
+import CurrentArenas from './pages/current-arenas/current-arenas.component.jsx';
 
 import './App.css';
 
@@ -9,6 +12,11 @@ import './App.css';
 const App = () => (
   <div className='app-bin'>
     <Header />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/login' component={SignInSignUp} />
+      <Route path='/arenas' component={CurrentArenas} />
+    </Switch>
   </div>
 );
 
