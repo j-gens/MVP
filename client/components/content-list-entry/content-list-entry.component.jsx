17ed30059ed30788cd08295c144bef9ccfcd9d02
j-gens <jgens2@gmail.com>
2020-03-11 13:20:42 -0700

    Boilerplate functional component and styles sheet

---
+++ client/components/content-list-entry/content-list-entry.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './content-list-entry.styles.css';
+
+
+const ContentListEntry = () => (
+  <tr>
+    here is a row
+  </tr>
+);
+
+
+export default ContentListEntry;
