e359a356b501fe64e9b016e44605cc241bf320db
j-gens <jgens2@gmail.com>
2020-03-11 10:35:22 -0700

    Turn CurrentArenas into a stateful component

--- client/pages/current-arenas/current-arenas.component.jsx
+++ client/pages/current-arenas/current-arenas.component.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
+import ContentList from '../../components/content-list/content-list.component.jsx';
+
 import './current-arenas.styles.css';
 
 
-const CurrentArenas = () => (
-  <div>
-    current arenas here
-  </div>
-);
+class CurrentArenas extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentArenas: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="current-arenas-bin">
+        <div className="current-arenas-image">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/2/28/NHL_arenas.jpg" alt="map with nhl arena locations" />
+        </div>
+        <div className="current-arenas-list">
+          <h2>Current Arenas:</h2>
+          <ContentList />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default CurrentArenas;
