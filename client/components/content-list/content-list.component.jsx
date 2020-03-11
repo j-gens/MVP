7c5c0bca54a2264f5fe597738162fcbc42ae025c
j-gens <jgens2@gmail.com>
2020-03-11 11:04:47 -0700

    Set up class names for styling and dynamically map table header

--- client/components/content-list/content-list.component.jsx
+++ client/components/content-list/content-list.component.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 import './content-list.styles.css';
 
 
-const ContentList = () => (
-  <div>
-    reusable list component
+const ContentList = ({ listHeaders, arenas }) => (
+  <div className="content-list-bin">
+    <table className="cl-table">
+      <thead className="cl-table-header">
+        <tr>
+          {listHeaders.map((header, index) =>
+            <td key={index}>{header.toUpperCase()}</td>
+          )}
+        </tr>
+      </thead>
+      <tbody className="cl-table-body">
+
+      </tbody>
+    </table>
   </div>
 );
 
