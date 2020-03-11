bff658e78794efe92c01376b56b84f691a782d83
j-gens <jgens2@gmail.com>
2020-03-10 17:31:14 -0700

    Rename files for more clarity

---
+++ client/components/content-list/content-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './content-list.styles.css';
+
+
+const ContentList = () => (
+  <div>
+    reusable list component
+  </div>
+);
+
+
+export default ContentList;
