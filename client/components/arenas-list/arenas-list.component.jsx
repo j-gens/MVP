cd5451734a384c79a2b69783d2135c413b4b034a
j-gens <jgens2@gmail.com>
2020-03-10 17:18:44 -0700

    Boilerplate funcitonal component and styles sheet

---
+++ client/components/arenas-list/arenas-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './arenas-list.styles.css';
+
+
+const ArenasList = () => (
+  <div>
+    reusable list component ?
+  </div>
+);
+
+
+export default ArenasList;
