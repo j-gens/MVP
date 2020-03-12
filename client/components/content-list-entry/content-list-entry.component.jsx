c354f76da18f2eeff505dc2b7877b3b93ce66136
j-gens <jgens2@gmail.com>
2020-03-12 12:19:57 -0700

    Add classnames for styling and complete table row layout

--- client/components/content-list-entry/content-list-entry.component.jsx
+++ client/components/content-list-entry/content-list-entry.component.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import './content-list-entry.styles.css';
 
 
-const ContentListEntry = () => (
-  <tr>
-    here is a row
+const ContentListEntry = ({ arena: { arenaName, team, city, stateProvince } }) => (
+  <tr className="content-list-row">
+    <td>{arenaName}</td>
+    <td>{team}</td>
+    <td>{city}</td>
+    <td>{stateProvince}</td>
   </tr>
 );
 
