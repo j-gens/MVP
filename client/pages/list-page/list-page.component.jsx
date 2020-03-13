c8b0ba930209e30c9f0a4c1e2c340dcd4c888bbb
j-gens <jgens2@gmail.com>
2020-03-12 19:54:15 -0700

    Boilerplate stateful component and styles sheet

---
+++ client/pages/list-page/list-page.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './list-page.styles.css';
+
+
+class ListPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        hello user
+      </div>
+    );
+  }
+}
+
+
+export default ListPage;
