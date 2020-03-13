e00e1a5e0a048d17f8ed6a109f8edc82f1b0e1e8
j-gens <jgens2@gmail.com>
2020-03-12 20:08:06 -0700

    Boilerplate stateful component and styles sheet

---
+++ client/components/list-form/list-form.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './list-form.styles.css';
+
+
+class ListForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        this is the form
+      </div>
+    );
+  }
+}
+
+
+export default ListForm;
