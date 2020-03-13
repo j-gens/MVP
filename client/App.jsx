bd5feb7643d4afc5e11e4a73f4a76942d75b45a2
j-gens <jgens2@gmail.com>
2020-03-12 19:59:45 -0700

    Add ListPage component to routing

--- client/App.jsx
+++ client/App.jsx
@@ -5,19 +5,23 @@ import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx';
 import CurrentArenas from './pages/current-arenas/current-arenas.component.jsx';
+import ListPage from './pages/list-page/list-page.component.jsx';
 
 import './App.css';
 
 
 const App = () => (
-  <div className='app-bin'>
+  <>
     <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/login' component={SignInSignUp} />
-      <Route path='/arenas' component={CurrentArenas} />
-    </Switch>
-  </div>
+    <div className='app-bin'>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/login' component={SignInSignUp} />
+        <Route path='/arenas' component={CurrentArenas} />
+        <Route path='/list' component={ListPage} />
+      </Switch>
+    </div>
+  </>
 );
 
 
