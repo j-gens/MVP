99d446ba06d66b7e2656d4da64c0559b43da6b7c
j-gens <jgens2@gmail.com>
2020-03-12 19:27:44 -0700

    Add classnames and additional content to the homepage

--- client/pages/homepage/homepage.component.jsx
+++ client/pages/homepage/homepage.component.jsx
@@ -5,7 +5,17 @@ import './homepage.styles.css';
 
 const HomePage = () => (
   <div className='homepage-bin'>
-    this is the homepage - welcome!
+    <div className='homepage-title'>
+      calling all hockey fans...
+      <span className='homepage-subtitle'>
+        ...can you watch a hockey game in all 31 NHL arenas?
+      </span>
+    </div>
+    <div className='homepage-phrase'>
+      <span className='homepage-subtitle'>
+       keep track of your progress as you complete the ICE BUCKET LIST &#8658;
+      </span>
+    </div>
   </div>
 );
 
