e279b7a5951702eb4d7c4c12afbd79239c10a45d
j-gens <jgens2@gmail.com>
2020-03-14 12:04:31 -0700

    Add additional classNames to update font styling

--- client/pages/homepage/homepage.component.jsx
+++ client/pages/homepage/homepage.component.jsx
@@ -6,14 +6,18 @@ import './homepage.styles.css';
 const HomePage = () => (
   <div className='homepage-bin'>
     <div className='homepage-title'>
-      calling all hockey fans...
-      <span className='homepage-subtitle'>
-        ...can you watch a hockey game in all 31 NHL arenas?
-      </span>
+      <div>
+        calling all <span className='homepage-marker'>hockey fans... </span>
+      </div>
+      <div>
+        <span className='homepage-subtitle'>
+          ...can you watch a hockey game in all 31 NHL arenas?
+        </span>
+      </div>
     </div>
     <div className='homepage-phrase'>
       <span className='homepage-subtitle'>
-       keep track of your progress as you complete the ICE BUCKET LIST &#8658;
+       keep track of your progress as you complete the <span className='homepage-marker'>ICE BUCKET LIST</span> &#8658;
       </span>
     </div>
   </div>
