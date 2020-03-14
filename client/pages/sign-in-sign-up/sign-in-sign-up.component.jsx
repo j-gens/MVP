2d4af76f93457736bb73d73b97ae4c928a7398c8
j-gens <jgens2@gmail.com>
2020-03-14 16:41:19 -0700

    Add additional classNames for spacing child components

--- client/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ client/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import SignUp from '../../components/sign-up/sign-up.component.jsx';
+
 import './sign-in-sign-up.styles.css';
 
 
 const SignInSignUp = () => (
-  <div className="signin-signup-bin">
-    sign in and sign up here
+  <div className='signin-signup-bin'>
+    <div className='signin'>
+      <SignIn />
+    </div>
+    <div className='signup'>
+      <SignUp />
+    </div>
   </div>
 );
 
