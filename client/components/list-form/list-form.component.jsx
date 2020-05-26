5e4f692d9e2c877e2a0e5278e4bb153415b26131
j-gens <jgens2@gmail.com>
2020-05-25 18:48:02 -0700

    Add input fields for form

--- client/components/list-form/list-form.component.jsx
+++ client/components/list-form/list-form.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component.jsx';
+
 import './list-form.styles.css';
 
 
@@ -7,14 +9,57 @@ class ListForm extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      arenaName: '',
+      homeTeam: '',
+      visitingTeam: '',
+      date: ''
     };
   }
 
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
+    const { arenaName, homeTeam, visitingTeam, date } = this.state;
     return (
-      <div>
-        this is the form
+      <div className='listform-bin'>
+        <form>
+          <span className='listform-title'>
+            I attended another game!
+          </span>
+          <FormInput
+            value={arenaName}
+            handleChange={this.handleChange}
+            type='text'
+            name='arenaName'
+            label='Arena Name'
+          />
+          <FormInput
+            value={homeTeam}
+            handleChange={this.handleChange}
+            type='text'
+            name='homeTeam'
+            label='Home Team'
+          />
+          <FormInput
+            value={visitingTeam}
+            handleChange={this.handleChange}
+            type='text'
+            name='visitingTeam'
+            label='Visitors'
+          />
+          <FormInput
+            value={date}
+            handleChange={this.handleChange}
+            type='date'
+            name='date'
+            label=''
+          />
+        </form>
       </div>
     );
   }
